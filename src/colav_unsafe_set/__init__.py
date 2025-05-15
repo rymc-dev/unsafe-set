3684f9c7303674eb8acdfb4cb86bfd1a3b080fe7
Ryan <r.mckee@qub.ac.uk>
2025-05-15 09:59:30 +0100

    adding collision metrics to __init__ __all__ access

--- src/colav_unsafe_set/__init__.py
+++ src/colav_unsafe_set/__init__.py
@@ -1,5 +1,7 @@
 from colav_unsafe_set.unsafe_set import create_unsafe_set
+from colav_unsafe_set.risk_assessment import calculate_obstacle_metrics_for_agent
 
 __all__ = [
-    'create_unsafe_set'
+    'create_unsafe_set',
+    'calculate_obstacle_metrics_for_agent'
 ]
